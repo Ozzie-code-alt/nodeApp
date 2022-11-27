af76a31a9c1bf64f1f887063d7bcdb4c9fe474a1
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-11-27 23:31:23 +0800

    Node Refreshner

---
+++ app.js
@@ -0,0 +1,19 @@
+// function sayHello(name){
+//     console.log(`Hello ${name}`);
+// }
+// // sayHello('Pedro');
+// console.log(window)
+
+
+// node Global objects
+
+/*console.log()
+setTimeout()
+clearTimeout()
+setInterval()
+clearInterval()
+
+
+in node variables are not declared as global soo use 
+global object for it to be read in node, otherwise it will be stuck to js
+*/
